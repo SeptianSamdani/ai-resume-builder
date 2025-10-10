e738b3818f083bd2097a137a2210c256523d60da
SeptianSamdany <227007072@student.unsil.ac.id>
2025-10-10 07:32:51 +0700

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-center p-5'>
+      <h1 className='text-3xl font-bold text-blue-300'>Resume AI Builder</h1>
+      <p>Build your resume with builder</p>
+
+      <div className="">
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
